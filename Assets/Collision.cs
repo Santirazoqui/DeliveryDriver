d9a1103c300ec3bcfd2c7ad4a4a02e2f218e6e43
Santirazoqui <irazoqui.santi@gmail.com>
2022-08-01 13:25:39 -0300

    Added collision event message

--- Assets/Collision.cs
+++ Assets/Collision.cs
@@ -4,15 +4,12 @@ using UnityEngine;
 
 public class Collision : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private void OnCollisionEnter2D(Collision2D other) 
     {
-        
+        Debug.Log("crunch");
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private void OnTriggerEnter2D(Collider2D other) {
+        Debug.Log("Hey! Listen!");
     }
 }
