4089aba7e0f16447497173e75b7285ae4d429aa0
Santirazoqui <irazoqui.santi@gmail.com>
2022-08-04 12:56:31 -0300

    Change color when package is picked and delivered

--- Assets/Delivery.cs
+++ Assets/Delivery.cs
@@ -4,8 +4,18 @@ using UnityEngine;
 
 public class Delivery : MonoBehaviour
 {
+    [SerializeField] Color32 hasPackageColor = new Color32 (1, 1, 1, 1);
+    [SerializeField] Color32 noPackageColor = new Color32 (1, 1, 1, 1);
+    //[SerializeField] Color32 hasPackageColor = new Color32 (1, 1, 1, 1);
+
     bool hasPackage;
     [SerializeField] int destroyDelay;
+
+    SpriteRenderer spriteRenderer;
+    private void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();   
+    }
 
     private void OnCollisionEnter2D(Collision2D other) 
     {
@@ -16,13 +26,16 @@ public class Delivery : MonoBehaviour
         if(other.tag == "Package" && !hasPackage)
         {
             Debug.Log("package picked up");
-            Destroy(other.gameObject, destroyDelay);
+            //SpriteRenderer packageSpriteRenderer = other.GetComponent<SpriteRenderer>();
+            spriteRenderer.color = hasPackageColor;//packageSpriteRenderer.Color;
             hasPackage = true;
+            Destroy(other.gameObject, destroyDelay);
         } 
         else if (other.tag == "Customer" && hasPackage == true)
         {
             Debug.Log("Package delivered");
             hasPackage = false;
+            spriteRenderer.color = noPackageColor;
         }
 
 
